6de33d1756b5bd17cebdc965cc99b10cc5685734
Sebastian Kühnau <seb@vaadin.com>
2025-12-15 10:17:03 +0100

    Add imports and placeholder comments in PlaygroundView, update styles to include theme-editor

--- src/main/java/com/vaadin/workshop/views/playground/PlaygroundView.java
+++ src/main/java/com/vaadin/workshop/views/playground/PlaygroundView.java
@@ -1,6 +1,9 @@
 package com.vaadin.workshop.views.playground;
 
 
+import com.vaadin.flow.component.button.Button;
+
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.PageTitle;
@@ -11,6 +14,13 @@ import org.vaadin.lineawesome.LineAwesomeIconUrl;
 @Route("playground")
 @Menu(order = 4, icon = LineAwesomeIconUrl.SLIDERS_H_SOLID)
 public class PlaygroundView extends VerticalLayout {
+
+    /**
+     * 1. Fundamentals Vaadin + Short exercise
+     * 2. Copilot + Exercise
+     * 3. Data Binding + Exercise
+     * 4. Testing + Exercise
+     */
 
     public PlaygroundView() {
 
